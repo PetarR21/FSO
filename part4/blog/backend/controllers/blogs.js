243755b5fc44a9f984a9aaa76f59a6a74b26db91
PetarR21 <petar.r.rakocevic@gmail.com>
2023-12-05 12:28:34 +0100

    Finished exercise 4.14

--- part4/blog/backend/controllers/blogs.js
+++ part4/blog/backend/controllers/blogs.js
@@ -19,4 +19,34 @@ router.post('/', async (request, response) => {
   response.status(201).json(savedBlog);
 });
 
+router.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
+  if (blog) {
+    response.json(blog);
+  } else {
+    response.sendStatus(404);
+  }
+});
+
+router.delete('/:id', async (request, response) => {
+  const blog = await Blog.findByIdAndDelete(request.params.id);
+  if (blog) {
+    response.sendStatus(204);
+  } else {
+    response.sendStatus(404);
+  }
+});
+
+router.put('/:id', async (request, response) => {
+  const blog = await Blog.findByIdAndUpdate(request.params.id, request.body, {
+    new: true,
+    runValidators: true,
+    context: 'query',
+  });
+  if (blog) {
+    response.json(blog);
+  } else {
+    response.sendStatus(404);
+  }
+});
 module.exports = router;
--- part4/blog/backend/tests/blog_api.test.js
+++ part4/blog/backend/tests/blog_api.test.js
@@ -88,6 +88,51 @@ describe('Blog API', () => {
     await api.post('/api/blogs').send(blogWithoutUrl).expect(400);
   });
 
+  test('backend returns individual blog correctly', async () => {
+    const blogs = await helper.blogsInDB();
+    const blog = await blogs[0];
+
+    const response = await api
+      .get(`/api/blogs/${blog.id}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    expect(response.body.title).toEqual(blog.title);
+  });
+
+  test('blog can be deleted', async () => {
+    const blogsAtStart = await helper.blogsInDB();
+    const blogToDelete = blogsAtStart[0];
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+    const blogsAtEnd = await helper.blogsInDB();
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
+
+    const titles = blogsAtEnd.map((b) => b.title);
+    expect(titles).not.toContain(blogToDelete.title);
+  });
+
+  test('blog can be updated', async () => {
+    const blogsAtStart = await helper.blogsInDB();
+    const blogToUpdate = blogsAtStart[0];
+
+    const response = await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send({ likes: blogToUpdate.likes + 1 })
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDB();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+
+    const blogs = blogsAtEnd.map((b) => {
+      return { title: b.title, likes: b.likes };
+    });
+    expect(blogs).toContainEqual({ title: blogToUpdate.title, likes: blogToUpdate.likes + 1 });
+  });
+
   afterAll(async () => {
     await mongoose.connection.close();
   });
