c8071b3c8174be1c585ec4858a96bfd7fb2dd701
PetarR21 <petar.r.rakocevic@gmail.com>
2023-12-05 22:45:12 +0100

    Started exercise 4.15

--- part4/notes/backend/controllers/notes.js
+++ part4/notes/backend/controllers/notes.js
@@ -1,4 +1,5 @@
 const notesRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
 const Note = require('../models/note');
 const User = require('../models/user');
 
@@ -7,10 +8,21 @@ notesRouter.get('/', async (request, response) => {
   response.json(notes);
 });
 
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '');
+  }
+  return null;
+};
+
 notesRouter.post('', async (request, response) => {
   const body = request.body;
-
-  const user = await User.findById(body.userId);
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' });
+  }
+  const user = await User.findById(decodedToken.id);
 
   if (!body.content) {
     return response.status(400).json({
@@ -21,7 +33,7 @@ notesRouter.post('', async (request, response) => {
   const note = new Note({
     content: body.content,
     important: body.important || false,
-    user: user.id,
+    user: user._id,
   });
 
   const savedNote = await note.save();
